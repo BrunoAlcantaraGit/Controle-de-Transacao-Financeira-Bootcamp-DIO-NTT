5b3e6eca655089c94db2a311f96e744f87311eb0
Bruno Alcantara <brunodeoliveiraalcantara@gmail.com>
2025-08-14 18:00:14 -0300

    Criação do repository InvestimentRepository com os metodos para alterar investimento, localizar investimento, e localizar carteirade investimentos

--- src/main/java/org/dio/repository/InvestimentRepository.java
+++ src/main/java/org/dio/repository/InvestimentRepository.java
@@ -1,9 +1,40 @@
 package org.dio.repository;
 
 import lombok.ToString;
+import org.dio.exception.InvestmentNotFoundException;
+import org.dio.exception.WalletNotFoundExeption;
+import org.dio.model.Investment;
+import org.dio.model.InvestmentWallet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ToString
 public class InvestimentRepository {
+
+    private final List<Investment> investments = new ArrayList<>();
+    private final List<InvestmentWallet> wallets = new ArrayList<>();
+
+    public void updateAmount(final long percet){
+        wallets.forEach(w->w.updateAmount(percet));
+    }
+
+
+    public Investment findbyID(final long id) {
+        return investments.stream()
+                .filter(i -> i.id() == id)
+                .findFirst()
+                .orElseThrow(
+                        () -> new InvestmentNotFoundException("nenhum investimento id:" + id + "Não encontrado")
+                );
+    }
+
+    public InvestmentWallet findWalletByAccountPix(final String pix) {
+        return wallets.stream()
+                .filter(w -> w.getAccount().getPix().contains(pix))
+                .findFirst()
+                .orElseThrow(() -> new WalletNotFoundExeption(" A carteira não foi encontrada"));
+    }
 
 
 }
